3cdd6ea8864184fb1ebd43f83ab906b95c1aeba4
bhavanikairika3 <147837665+bhavanikairika3@users.noreply.github.com>
2023-10-13 23:01:27 +0530

    Create an array.java

---
+++ Create an array.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+public class ShuffleArray {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> list = Arrays.asList(arr);
+        Collections.shuffle(list);
+        System.out.println("Shuffled Array: " + list);
+    }
+}
