c428ca728e7ff3c665e04f7d78f3891acb126735
bhavanikairika3 <147837665+bhavanikairika3@users.noreply.github.com>
2023-10-14 10:00:41 +0530

    code.java

--- code.java
+++ code.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-public class ShuffleArray {
-    public static void main(String[] args) {
+public class ShuffleArray 
+{
+    public static void main(String[] args)
+    {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
         List<Integer> list = Arrays.asList(arr);
         Collections.shuffle(list);
